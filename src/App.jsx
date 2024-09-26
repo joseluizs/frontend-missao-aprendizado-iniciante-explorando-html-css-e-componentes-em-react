8433d4fdd1a15f1f9181354628740b08c0a8d432
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-09-26 11:42:41 +0000

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <h1>Java</h1>
+        <img src="https://www.salvatore.academy/devmon/1_java.png" width="200px" />
+      </div>
+    </>
+  )
+}
+
+export default App
